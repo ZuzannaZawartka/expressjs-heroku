6302760cf1a914878c0ce57d9a457150e3a0b995
Zuziaxy <raf.zuzia7@gmail.com>
2021-12-28 20:33:48 +0100

    added /data to get

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku - zmiana!</h1>")
 })
 
+
+app.get("/data", function (req, res) {
+    obj = {imie : "Zuzanna", nazwisko: "Zawartka",klasa:"3i2",grupa:"1"}
+    res.send(obj)
+})
+
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
